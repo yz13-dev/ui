15714967955dbdc1fbf5e06995536ccf86310438
yz13-dev <yztheceo@yandex.ru>
2026-09-06 22:01:42 +0500

    update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import { Provider } from '@/components/provider';
 import { cn } from '@/lib/utils';
+import { mono, pixel, sans, serif } from '@/registry/lib/fonts';
 import type { Viewport } from 'next';
-import { sans, mono, serif, pixel } from '@/registry/lib/fonts';
 import "../registry/globals.css";
 import './global.css';
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 export const viewport: Viewport = {
   colorScheme: "light dark",
@@ -26,6 +28,16 @@ export default function Layout({ children }: LayoutProps<'/'>) {
       className={cn(sans.variable, mono.variable, serif.variable, pixel.variable)}
       suppressHydrationWarning
     >
+      <head>
+        {isProduction && (
+          <script
+            async
+            src="https://c.analytics.yz13.dev/oa.js"
+            data-key="oa_pk_Ky9h5OKmp22ydLxC2A_50zKRrVz5J1a3"
+            data-collector="https://c.analytics.yz13.dev"
+          />
+        )}
+      </head>
       <body id="root" className="flex flex-col antialiased min-h-screen">
         <Provider>{children}</Provider>
       </body>
